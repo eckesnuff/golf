5307cc8d7fd7cfbfec6e93f0d2eaf888b38d75ba
Erik Hammarström <eckesnuff@gmail.com>
2020-04-05 17:57:08 +0200

    Fix validation

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -25,7 +25,9 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).ConfigureApiBehaviorOptions(x=>{
+                x.SuppressModelStateInvalidFilter=true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
